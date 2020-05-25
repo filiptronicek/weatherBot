1da0369b4ae7fdea5316b362c5cfc8f849ead7bd
Filip Troníček <filip.tronicek@seznam.cz>
2020-05-25 14:33:48 +0200

    Add requirements

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from pyowm import OWM
-import os
-from creds import API_key, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
 from twython import Twython
+from os import environ
+
+if environ.get('API_key') is not None:
+    from creds import API_key, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+else:
+    API_key = environ.get('API_key')
+    CONSUMER_KEY = environ.get('CONSUMER_KEY')
+    CONSUMER_SECRET = environ.get('CONSUMER_SECRET')
+    ACCESS_KEY = environ.get('ACCESS_KEY')
+    ACCESS_SECRET = environ.get('ACCESS_SECRET')
 
 owm = OWM(API_key)
 obs = owm.weather_at_place('Prague,CZ')
@@ -13,7 +21,7 @@ twitter_api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 tstatus = ""
 
 if weather == "sunny":
-    tstatus = "Yes, it is sunny @vlada_svoboda @filiptronicek"
+    tstatus = "Yes, it is sunny"
 else:
-    tstatus = "No, the weather is "+weather+" @vlada_svoboda @filiptronicek"
-twitter_api.update_status(status=tstatus)
+    tstatus = "No, the weather is "+weather
+#twitter_api.update_status(status=tstatus)
